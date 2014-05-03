8ad30a6c1c8f904361c8c89accf615afc87f5f43
Matija Rezar <mr3193@student.uni-lj.si>
2014-05-03 12:31:59 +0200

    scr-55 REST vmesnik za dodajanje časa dela

---
+++ scrumkin-rs/src/main/java/com/scrumkin/rs/json/TaskWorkDoneJSON.java
@@ -0,0 +1,31 @@
+package com.scrumkin.rs.json;
+
+import com.scrumkin.jpa.TaskEntity;
+import com.scrumkin.jpa.TasksWorkDoneEntity;
+import com.scrumkin.jpa.UserEntity;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+/**
+ * Created by Matija on 3.5.2014.
+ */
+public class TaskWorkDoneJSON {
+
+    public int id;
+    public int user;
+    public int task;
+    public double workDone;
+    public double workRemaining;
+    public Timestamp date;
+
+    public void init(TasksWorkDoneEntity tasksWorkDoneEntity) {
+        this.id = tasksWorkDoneEntity.getId();
+        this.user = tasksWorkDoneEntity.getUser().getId();
+        this.task = tasksWorkDoneEntity.getTask().getId();
+        this.workDone = tasksWorkDoneEntity.getWorkDone().doubleValue();
+        this.workRemaining = tasksWorkDoneEntity.getWorkRemaining().doubleValue();
+        this.date = tasksWorkDoneEntity.getDate();
+    }
+
+}
